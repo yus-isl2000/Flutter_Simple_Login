6c3b478687adf0f9f9768394374c3db0e735e9a1
yus-isl2000 <60628472+yus-isl2000@users.noreply.github.com>
2020-10-24 19:19:50 -0400

    Update success.dart

--- lib/success.dart
+++ lib/success.dart
@@ -6,6 +6,28 @@ class SuccessPage extends StatefulWidget {
 }
 
 class _SuccessPageState extends State<SuccessPage> {
+  final numberFilter = TextEditingController();
+  List users = ['Yusef', 'John', 'Sarah'];
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    numberFilter.dispose();
+    super.dispose();
+  }
+
+  String printList(List stuff, int number) {
+    String result = 'These are the users: \n';
+    if (number <= stuff.length) {
+      for (int i = 0; i <= number - 1; i++) {
+        result += stuff[i] + '\n';
+      }
+      return result;
+    } else {
+      return 'Give me a better number pls :)';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,10 +45,38 @@ class _SuccessPageState extends State<SuccessPage> {
                 fontSize: 20,
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                child: TextField(
+                  controller: numberFilter,
+                  decoration: new InputDecoration(labelText: 'Number'),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              // When the user presses the button, show an alert dialog containing
+              // the text that the user has entered into the text field.
+              onPressed: () {
+                return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      // Retrieve the text the that user has entered by using the
+                      // TextEditingController.
+                      content:
+                          Text(printList(users, int.parse(numberFilter.text))),
+                    );
+                  },
+                );
+              },
+              tooltip: 'Show me the value!',
+              child: Icon(Icons.text_fields),
+            ),
             Image.asset(
               'images/smile.png',
-              width: 250,
-              height: 250,
+              width: 100,
+              height: 100,
             )
           ],
         ),
